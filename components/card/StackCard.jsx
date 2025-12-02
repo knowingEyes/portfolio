db71c6ee639556f0cf2caf292b50d85db11c3fb1
Oluwa Ibrahim <mj9762651@gmail.com>
2025-12-02 17:39:17 +0100

    Enable scroll to last viewed position when showing less stacks

--- components/card/StackCard.jsx
+++ components/card/StackCard.jsx
@@ -2,17 +2,29 @@
 import { useState } from "react";
 import Button from "../ui/Button";
 
-const { STACKS_DATA } = require("@/data/stacks");
-const { default: Card } = require("../ui/Card");
+import { STACKS_DATA } from "@/data/stacks";
+import Card from "../ui/Card";
 
 const StackCard = () => {
-  const [more, setMore] = useState(false);
-  // show only first 4 items if more is false
-  const stacks = more ? STACKS_DATA : STACKS_DATA.slice(0, 4);
+  const [showMore, setShowMore] = useState(false);
+
+  //
+  const handleShowLess = () => {
+    const savedPosition = [...document.querySelectorAll(".item")][3].offsetTop;
+    console.log(savedPosition);
+    if (savedPosition) {
+      window.scrollTo({ top: savedPosition * 2, behavior: "smooth" });
+      setShowMore(false);
+    }
+  };
+
+  // show only first 4 items if ShowMore is false
+  const stacks = showMore ? STACKS_DATA : STACKS_DATA.slice(0, 4);
+
   return (
-    <div className="grid gap-6">
+    <div className="grid gap-4">
       {stacks.map((item, i) => (
-        <Card key={i} className="flex flex-col justify-center min-h-40">
+        <Card key={i} className="flex flex-col justify-center min-h-40 item">
           <div
             className="mb-4 inline-flex p-3 rounded-lg bg-gray-950 border border-gray-800 w-fit group-hover:scale-[1.09]
         group-hover:border-white transition-transform cursor-pointer duration-500"
@@ -30,9 +42,9 @@ const StackCard = () => {
       <Button
         className="w-max"
         variant="outline"
-        onClick={() => setMore(!more)}
+        onClick={!showMore ? () => setShowMore(true) : handleShowLess}
       >
-        View More
+        {showMore ? "Show less" : "Show more"}
       </Button>
     </div>
   );
